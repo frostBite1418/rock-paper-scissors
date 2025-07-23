bdee33a03f2709667bf9c1cee292ba77987622e2
Primo <71015576+frostBite1418@users.noreply.github.com>
2025-07-23 19:25:52 +0800

    Create pseudocode and program initial human and computer score

---
+++ javascript.js
@@ -0,0 +1,38 @@
+// Play one game of rock-paper scissors
+
+// Set up computer score
+let computerScore = 0;
+
+// Set up human score
+let humanScore = 0;
+
+// Ask for computer input from one to three
+// One is for paper; Two is for rock; Three is for scissors
+
+// Ask for human input: rock, paper, or scissors
+// Make sure it is case-insensitive
+// Ask to retype if their answer is invalid
+
+// Compare human input from computer input
+    // if human input and computer input is the same, it is a tie
+     // alert tie
+     // no points added
+    // Write winning condition 1: paper vs rock
+        // alert you won
+        // add points
+    // Write winning condition 2: scissors vs paper
+        // alert you won
+        // add points
+    // Write winning condition 3: rock vs scissors
+        // alert you won
+        // add points
+    // Else, you lose and computer gains a point;
+        // alert you lose
+        // add computer points
+
+// Create a while loop until any score reaches five
+    // if human score equals to five
+        // alert you win
+    // else
+        // you lose
+
